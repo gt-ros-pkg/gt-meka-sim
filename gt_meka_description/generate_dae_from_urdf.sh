5d9e7a69805501015c30564223355e626083a94b
pgrice <phillip.grice@gatech.edu>
2014-02-17 14:04:21 -0500

    adding robot name as arg to generate scripts

--- gt_meka_description/generate_dae_from_urdf.sh
+++ gt_meka_description/generate_dae_from_urdf.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
+## Takes the name of the robot as a single argument (ex. "./generate_dar_from_urdf.sh darci")
+
 # Generate dae from URDF
-rosrun collada_urdf urdf_to_collada ./urdf/darci.urdf ./collada/darci.dae 
+rosrun collada_urdf urdf_to_collada ./urdf/$1.urdf ./collada/$1.dae 
 
 # If joint axes/anchors have pretty big decmials, round off those (using moveit_ikfast's script).
-rosrun gt_meka_description round_collada_numbers.py ./collada/darci.dae ./collada/darci.rounded.dae 3
+rosrun gt_meka_description round_collada_numbers.py ./collada/$1.dae ./collada/$1.rounded.dae 3
