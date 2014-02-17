4407313022e0f45d857e7405c68b7bbfda1af319
pgrice <phillip.grice@gatech.edu>
2014-02-17 13:24:41 -0500

    merging changes from up to date hrl internal simulator package, including new arm controller gain files, and new meshes

---
+++ hrl_gazebo_darci/generate_dae_from_urdf.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Generate dae from URDF
+rosrun collada_urdf urdf_to_collada ./urdf/darci.urdf ./collada/darci.dae 
+
+# If joint axes/anchors have pretty big decmials, round off those (using moveit_ikfast's script).
+rosrun hrl_gazebo_darci round_collada_numbers.py ./collada/darci.dae ./collada/darci.rounded.dae 3
