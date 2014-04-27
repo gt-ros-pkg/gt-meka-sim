3ec2cd3c0e596ef48dfde27947bf66b8920fba4a
vchu <vchu@gatech.edu>
2014-04-27 19:02:59 -0400

    added in more edits for arm and hand control

---
+++ gt_meka_description/src/siml_gazebo_curi/body_part_setup.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+import roslib; roslib.load_manifest('gt_meka_description')
+import rospy
+from trajectory_msgs.msg import JointTrajectory, JointTrajectoryPoint
+from pr2_controllers_msgs.msg import JointTrajectoryControllerState
+import sys
+
+class JointTrajectoryTest():
+    def __init__(self, controller='/r_arm_controller'):
+        self.controller = controller
+        self.goal_pub = rospy.Publisher(controller+'/command', JointTrajectory)
+        self.state_sub = rospy.Subscriber(controller+'/state', JointTrajectoryControllerState, self.state_cb)
+        self.joint_names = None
+
+    def state_cb(self, state_msg):
+        if self.joint_names is None:
+            self.joint_names = state_msg.joint_names
+
+    def head_up_msg(self):
+        jtm = JointTrajectory()
+        jtm.joint_names = self.joint_names
+        jtp = JointTrajectoryPoint()
+        jtp.positions = [0]*len(self.joint_names)
+        jtp.time_from_start = rospy.Duration(1.0)
+        jtm.points = [jtp]
+        return jtm
+
+    def run(self):
+        while self.joint_names is None:
+            print "Waiting for joint state information from %s/state topic" %self.controller
+            rospy.sleep(2)
+        print "Received joint state information. Sending neck to default position (0.0m)"
+        head_up_msg = self.head_up_msg()
+        self.goal_pub.publish(head_up_msg)
+
+if __name__=='__main__':
+    rospy.init_node('simco_arm_setup')
+    if (len(sys.argv) > 1):
+        controller = sys.argv[1]
+    else:
+        controller = 'r_arm_controller'
+    JTT = JointTrajectoryTest(controller=controller)
+    JTT.run()
